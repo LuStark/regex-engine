15f1698ac999b6339d2121b4bd413d29898a008d
LuStark <lance5158387@gmail.com>
2014-07-01 13:02:50 +0800

    不采取多个正则表达式

--- bin/LLex.h
+++ bin/LLex.h
@@ -5,8 +5,6 @@
 #include "DFAEntity.h"
 #include <stdio.h>
 
-/* lexfile必须要进行转义的字符： {  } 
- */
 
 #define     HEADER_MAX  500
 #define     LINE_MAXSIZE 100
@@ -15,68 +13,53 @@
 
 #define     MAXSIZE_HASH    1500
 
-/* 首先我们定义lexfile中诸如 Number  {digit}|{float}|0123 的整条记录为 实体
- * 一个实体中可以存在几个正则表达式，如Number实体就有三个正则表达式：
- *    digit, float, "0123"    ,其中{ name } 表示先前定义的正则表达式
- */
-
-/* RegExpInfo 储存一个实体中的一条正则表达式，它可能是先前定义的，
- * 也可能是现有实体中具体的表达式（如0123）
- */
-
-typedef struct RegexInfo{
-    char        *name;              /* 具体正则式取名：  实体名字 + _concrete_ + 编号 */
-    char        *concrete_rExp;     /*具体的正则表达式，如0123*/
-    DFAEntity   *dfa_entity;  
-    int         ReverseMatch;       /* 取反标志， 例如 {~digit}就是不匹配digit的任意串 */
-}RegexInfo;
-
-
-/* struct MatchEntity 代表每个实体
+/* struct RegexEntity 代表每个实体
  * 例如实体：      FLOAT    (+|-)?{number}.{number}E
    
                  { FLOAT } { printf("FLOAT"); }
 
- * MatchEntity以下字段分别储存此条记录的如下信息：
- *      1. entityName是lexfile中定义的名字， 此处 entityName = "FLOAT"
+ * RegexEntity以下字段分别储存此条记录的如下信息：
+ *      1. name是lexfile中定义的名字， 此处 name = "FLOAT"
  *      2. action 是 匹配后的执行代码
- *      3. RegexArray 记录 实体中所有正则式，这里包括 (+|-)? 、 number 、. 、number 还有 E
+ *      3. Regex 记录实体的正则式，Regex中可能包含以命名的正则表达式
+ *               Regex正则表达式中已命名的正则表达式最后会展开：
+ *             如 FLOAT     (+|-)?[0-9]+.[0-9]+E
+ *          
+ *          如果{}内的名字不存在，则保留原来的字串:
+ *             如 Comment     {%.*%}
  */
 
-
-typedef struct _match_entity{
-    char        *entityName;
+typedef struct _regex_entity{
+    char        *name;
     char        *action;
+    xchar_t     *regex;
+    DFAEntity   *dfa_entity;
+}RegexEntity;
 
-    RegexInfo   RegexArray[100];
-    int         numOfRegexArray;
-
-    char        op[100];    /* op[i]表示 Regex[i]和Regex[i+1]的连结符号： | & */
-}MatchEntity;
-
-/* 利用二叉树储存所有实体，实体的名字（entityName）作为关键码
+/* 利用二叉树储存所有实体，实体的名字（name）作为关键码
  * 小于关键码的作为左子结点，反之作为右子结点
  */
 typedef struct _node {
     char            *key;
-    MatchEntity     match_entity;
+    RegexEntity     regex_entity;
     struct  _node   *lChild;
     struct  _node   *rChild;
 }Node, *linkNode;
 
 /* 初始化“实体树”，返回根结点 */
 linkNode
-*Init_EntityTree( MatchEntity ent );
+*Init_EntityTree( RegexEntity ent );
 
 /* 往树中插入实体, 若已存在该实体（名字相同），return false */
 bool
-Insert_EntTree( linkNode root, MatchEntity ent );
+Insert_EntTree( linkNode root, RegexEntity ent );
 
 /* 寻找给定名字的实体 */
 linkNode
 *Find( linkNode root, char *name );
 
-
+xchar*
+getRegexFromNode( linkNode );
 
 
 void    checkDFA_AND_text(
@@ -85,18 +68,17 @@ void    checkDFA_AND_text(
                         );
 
 
-/* 对于正则表达式MatchEntity中的DFA，过程generate_FinalStatusJudge
+/* 对于正则表达式RegexEntity中的DFA，过程generate_FinalStatusJudge
  * 为该DFA产生一个终点判别数组 FinalStatus[], 其中FinalStatus[i]
  * 为true表示 DFA的第i个状态是终点接受状态（i=0,1,2,...）
  */
-void    generate_FinalStatusJudge( FILE *, MatchEntity );
+void    generate_FinalStatusJudge( FILE *, RegexEntity );
 
 
-/* 为MatchEntity中的DFA产生一个状态转换表T， 其中，
+/* 为RegexEntity中的DFA产生一个状态转换表T， 其中，
  * T[i][j] 表示DFA的第i个状态，在字符j（ASCII值）下能转换到的状态号 
  */
-void    generate_T( FILE *, MatchEntity );
-
+void    generate_T( FILE *, RegexEntity );
 
 
 /* 为每个具体正则表达式产生一个识别函数，该识别函数的原型是
@@ -104,58 +86,29 @@ void    generate_T( FILE *, MatchEntity );
  * 判别函数利用表T_%s（全局变量）执行一系列状态转换，若最终得到
  * 的状态是接受状态，Recognize_for_%s返回true 
  */
-void    generate_Concrete_Recognize( FILE *f, RegexInfo regex );
-
-/* 为每个识别实体产生一个识别函数，原型是bool Recognize_for_%s( char *text );
- * 对于任意串text, 识别实体利用其储存的若干个正则表达式的逻辑运算进行综合：
- * 例如:  FLOAT    (+|-)?{number}.{number}F
- * 则 bool Recognize_for_FLOAT( char *text )
- *    {
- *         if( 
-                Recognize_for_FLOAT_concrete1( text ) &&
-                Recognize_for_number( text ) &&
-                Recognize_for_FLOAT_concrete2( text ) &&
-                Recognize_for_number( text ) &&
-                Recognize_for_FLOAT_concrete3( text )
-           )
-            {
-                ...
-            }
-      }
- */
-void    generate_MatchEntity_Recognize( FILE *f, MatchEntity match_entity ); 
+void    generate_Regex_Recognize( FILE *f, RegexEntity regex_entity ); 
 
 
 
-/* 为MatchEntity产生识别和执行相应动作的函数 */
-void    generate_Recognize_Act( FILE *, MatchEntity );
+/* 为RegexEntity产生识别和执行相应动作的函数 */
+void    generate_Recognize_Act( FILE *, RegexEntity );
 
 void    generate_HeaderInfo( FILE *, char [] ); 
 
-/* 读取字符串，并且将所有正则表达式记录于MatchEntity类型的数组中,
- * 将所有头定义信息填入char数组中 
+/* 读取字符串，并且将所有正则表达式记录于linkNode类型的根结点上
+ * 将所有头定义信息填入char数组中
  */
-void    ReadLexFile( char *filename, MatchEntity [], char HeaderDef[]);
+linkNode    
+ReadLexFile( char *filename, char HeaderDef[]);
 
-void    read_Regular_Action( FILE *f, MatchEntity [] );
+void    read_Regular_Action( FILE *f, RegexEntity [] );
 
-
-/* Array[]是散列槽，有许多空位置，而clear_null_slot的用途就是将
- * Array[]中所有的非空槽写进newArray, 而此后不再检索某个特定的MatchEntity
- */
-void    
-clear_null_slot( MatchEntity Array[],
-                 MatchEntity newArray[],
-                 int *
-            );
-
-/* 为MatchEntity中所有的正则表达式构造相应的DFA以及状态转换表T */
-void construct_DFA(MatchEntity [], int);
+/* 为RegexEntity中所有的正则表达式构造相应的DFA以及状态转换表T */
+void construct_DFA(RegexEntity [], int);
 
 
-
-/* 为MatchEntity数组中的每一个DFA分别构造： 终点状态判别表，状态转换表，识别函数 */
-void generate_everything( FILE *, MatchEntity[], int, char[] );
+/* 为RegexEntity数组中的每一个DFA分别构造： 终点状态判别表，状态转换表，识别函数 */
+void generate_everything( FILE *, RegexEntity[], int, char[] );
 
 /* 产生指向一个指针函数数组，每个指针分别指向一个Regexp对应的RecognizeAct_for_%s函数。
  * 以后当识别一个token时，将这个token分别与这个数组中每个函数进行识别
@@ -163,7 +116,7 @@ void generate_everything( FILE *, MatchEntity[], int, char[] );
 void generate_function_array( FILE *f, int numOfArray );
 
 /* 让函数指针数组中每一个元素ptr[i]分别指向 Array[]中每个元素的RecognizeAct_for_%s */
-void generate_all_recognize_functions( FILE *f, MatchEntity Array[], int numOfArray );
+void generate_all_recognize_functions( FILE *f, RegexEntity Array[], int numOfArray );
 
 
 #endif
