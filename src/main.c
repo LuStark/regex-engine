f0412980dd8e08fe29c054344ab938197b73443f
LuStark <lance5158387@gmail.com>
2014-07-05 15:00:38 +0800

    程序入口

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
-#include "global.h"
+#include "LLex.h"
+#include <stdio.h>
+#include <string.h>
 
 xchar_t *RegExp[ MAXNUM_REGEXP ];
 
@@ -7,38 +9,45 @@ Index    current_RegExpArray_Index ;
 
 int  numOfRegExps;
 
-int main( )
+int main(int argc, char *argv[])
 {
-    int i, j ;
-    xchar_t     newRegExp[100] ;
-    NFA *nfa, *nfa2, *nfa3, *dfa;
-    StatusSet   s1,s2,S[8];
-    Condition   CondSet[100];
-    int     sizeofCondSet;
-//    HashTable_For_Wrap  HT;
+    int     numOfArray;
+    FILE    *file_lexyyc;
+    char    HeaderDef[ HEADER_MAX ];
+    char    fileName[100];
+    RegexEntity     Array[200];
 
-//    Scan( "reg_exam1.txt" );
-    //nfa = CharRange( RegExp[0], 0 );
-//    for( i=0; i<1; i++ )
-    wprintf(L"Regular Expression detecting: \n");
-    wprintf(L">>>  ");
-    while( wscanf(L"%ls",newRegExp) != EOF )
+    file_lexyyc = fopen("lex.yy.c","w");
+    
+    if( !file_lexyyc )
     {
-        nfa = RegexpToNFA( newRegExp ) ;
-        if( !nfa ){
+        printf("无法打开lex.yy.c文件。\n");
+        exit(1);
+    }
+
+    if( argc < 2 )
+    {
+        scanf("%s", fileName);
+        if( strlen(fileName) >= 100 )
+        {
+            printf("字符串长度超过额定限制，退出。\n");
             exit(1);
         }
-        wprintf(L"NFA graph:\n");
-        printNFA( nfa );
-        //getchar();
-    
-        dfa = Subset_Construct( nfa );
-        wprintf(L"DFA graph:\n");
-        printNFA( dfa );
-
-        wprintf(L">>>  ");
     }
-    /* 无法识别的正则表达式: */
-    /* b(a) */
+    else
+    {
+        strcpy( fileName, argv[1] );
+    }
+
+    linkNode root = ReadLexFile( fileName, HeaderDef );
+
+    numOfArray = 0;
+    StoreInList( root, Array, &numOfArray );
+
+    construct_DFA( Array, numOfArray );
+
+    generate_everything( file_lexyyc, Array, numOfArray, HeaderDef );
+
+    fclose( file_lexyyc );
 
 }
