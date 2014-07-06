94780b20bf628ff22a154856fc222d4798bb387f
LuStark <lance5158387@gmail.com>
2014-07-06 10:43:00 +0800

    补充注释

--- src/DFAEntity.h
+++ src/DFAEntity.h
@@ -1,3 +1,8 @@
+/* 
+ * DFAEntity是一个包含有自动机，表格，名字，状态数的结构体
+ */
+
+
 #ifndef _DFAENTITY_H_
 #define _DFAENTITY_H_
 
@@ -13,9 +18,11 @@ typedef struct dfa_entity {
     int         numOfStatus;
 } DFAEntity;
 
+/* DFA自动机可以生成一个二维表, 也称为状态转换表 */
 DFATable
 makeUpDFATable( DFA *dfa );
 
+/* 识别函数可以针对一个串进行识别，返回true/false */
 bool
 Recognition( DFAEntity entity, xchar_t *normal );
 
