904b33921c9af56e8c6f1a462e93c93b91f64869
LuStark <lance5158387@gmail.com>
2014-09-04 20:14:45 +0800

    轮子

---
+++ src/ArrayRep.h
@@ -0,0 +1,17 @@
+#ifndef ARRAYREP_INCLUDED
+#define ARRAYREP_INCLUDED
+
+#include "Array.h"
+#define T Array_T
+
+struct T {
+    int length;
+    int size;
+    char *array;
+};
+
+extern void ArrayRep_init(T array, int length,
+            int size, void *ary);
+
+#undef T
+#endif
