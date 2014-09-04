38dbe89c02d04fb12cd5b8ecac6b28ea1ac7837a
LuStark <lance5158387@gmail.com>
2014-09-04 20:13:54 +0800

    重构声明

--- src/NFA.h
+++ src/NFA.h
@@ -4,135 +4,95 @@
 #include "typedef.h"
 #include <stdbool.h>
 
-typedef struct Status Status;
-typedef struct Edge Edge;
 typedef struct NFA NFA;
 typedef Status *VertexTable; 
-typedef Edge    *Condition;
 typedef NFA     DFA;
+typedef NFA     *pt_NFA;
 
-struct Status 
-{
-    Edge **InEdges;
-    int numOfInEdges;
-    Edge **OutEdges;
-    int numOfOutEdges;
-    bool FinalStatus;
+// -----------------Edge 重构区 -------------------
+#define E Edge
+typedef struct E *E;
 
-    int ID;
-};
-
-struct Edge {
-    /* matchRange 和 inMatchRange的内容不一定互补。
-     * 范围的指定是由正则表达式 如[A-Z] 和 [^A-Z] 确定.
-     * inMatchContent 的内容 可由matchContent复制。如：
-      identifier = abc;    
-      digit = ~identifier. 则digit中inMatchContent可由identifier复制.
-    */
-
-    Status *from_Status;
-    Status *to_Status ;
-
-    Range *matchRange ;
-    int numOfMatchRange ;
-
-    /* 凡是匹配matchContent字符串中的一个字符
-     * 的字符，都能通过此边进行 转换 */
-    xchar_t *matchContent ;
-    /* 凡是不匹配inMatchContent字符串中的一个字符
-     * 能通过此边进行 转换 */
-    xchar_t *inMatchContent ;
-
-    Range *inMatchRange ;   
-    int numOfInMatchRange ;
-    bool AllowedAnyChar;  /* 支持任意字符, 以正则表达式中的 . 表示 */
-    bool hasEpsilon ;    /* 改变是否支持 epesilon 转换.*/ 
-
-};
-
-struct NFA 
-{
-    Status *start, *end;    /* NFA只有一个end,而DFA有多个end,所以DFA不使用end字段 */
-    Edge **edgeArray ;
-    Status **Status ; /* 首尾分别指向start,end */
-    int numOfStatus ;
-    int numOfEdges ;
-};
-
-/* 连结前后两个NFA */
-extern NFA *Link( NFA* , NFA* );
-
-/* 并置前后两个NFA */
-extern NFA *Union( NFA *, NFA * );
-
-/* 闭包(重复0零或1次以上) */
-extern NFA *Closure( NFA * );
-
-/* 重复1次或以上 */
-extern NFA *Repeat_atleast_one( NFA * );
-
-/* 可选的正则表达式 */
-extern NFA *Option( NFA * );
-
-/* 用一条边(edge)连结两个状态(status) */
-
-void 
-linkTwoStatus_by_AnEdge( 
-            Status * , 
-            Status * ,
-            Edge   *
-            );
+extern void outputEdgeCrossTable (E e);
+extern E    allocEdge ();
+extern E    addCharacter (E e, wchar_t c);
+extern void addRange (E e, Range r);
+extern int  crossEdge (E e, wchar_t c);
+extern void setMatchRangeOrNot (E e, int);
 
 /* 产生一条epsilon边 */
-Edge*
-allocEpsilonEdge( );
+E    allocEpsilonEdge();
+
+extern Edge allocEmptyEdge ();
+extern bool EqualCondition (E, E); 
+void printEdge (E);
+E CopyEdge (E);
+void freeEdge( E edge );
+
+//-------------------Status 重构区--------------------
+
+#define S   Status
+typedef struct S *S;
 
 /* 产生一个状态点 */
-Status*
-allocStatus( ) ;
+extern S    allocStatus ();
 
-extern xchar_t
-*PreprocessExpression( xchar_t *RegExp );
+extern void outputStatus (S);
+// 调试专用函数
+extern Status   nextStatus (S, wchar_t);
 
-NFA *RegexpToNFA( 
-        xchar_t  *RegExp
-        );
+extern void     printStatus (S s);
 
-/* 不接受任何转义字符 */
-NFA *CharRange( xchar_t *RegExp, int start_offset );
+/* ------------------NFA 重构区 --------------------- */
 
-NFA *CreateOneNFA( xchar_t c );
+typedef struct NFA  *NFA;
+
+/* 根据匹配单个的字符创建只有一条边两个状态的NFA */
+extern NFA  CreateOneNFA (wchar_t c);
+
+/* 连结前后两个NFA */
+extern NFA  Link (NFA, NFA);
+
+/* 并置前后两个NFA */
+extern NFA  Union (NFA, NFA);
+
+/* 闭包(重复0零或1次以上) */
+extern NFA  Closure (NFA);
+
+/* 重复1次或以上 */
+extern NFA  Repeat_atleast_one (NFA);
+
+/* 可选的正则表达式 */
+extern NFA  Option (NFA);
+
+/* 用一条边(edge)连结两个状态(status) */
+void 
+linkTwoStatus_by_AnEdge (Status from, Status to, Edge bridge);
+
+
+
+
+extern NFA CreateOneNFA (wchar_t c);
 
 /* 创建只有一条边，但无任何属性值的边。 */
-NFA *CreateNullNFA( );
+extern NFA CreateNullNFA ();
 
-// 调试专用函数
-Status *nextStatus( Status *currStatus,
-                   xchar_t recvChar
-                );
 
-void printEdge( Edge * );
 
-bool isMatchInEdge( xchar_t c,
-                    Edge   *e
+bool isMatchInEdge( wchar_t c,
+                    E   e
                     );
 
-void printStatus( Status *s );
-void printNFA( NFA * );
+void printNFA   (NFA );
 
-void EdgeArrayAppend(Edge ***,int*, Edge *);
-
-NFA *CopyNFA( NFA * );
-
-Edge *CopyEdge( Edge * );
+NFA  CopyNFA (NFA);
 
 
 /* 第三个参数: 数组大小; */
-void RangeCopy( Range* ,Range *,int );
+void RangeCopy (Range* ,Range *,int );
 
-void outputStatus( Status * );
 
-void isNullPointer( void * );
+void isNullPointer (void *);
 
 
 /* epsilon closure */
@@ -141,21 +101,16 @@ bool
 itemInSet( int id, int Set[], int size );
 
 void 
-generateCondSet(NFA *,Condition*, int* );
-
-bool
-isMatchCondition( Condition, xchar_t );
+generateCondSet (NFA, E, int* );
 
 
-extern bool
-EqualCondition( Condition , Condition); 
 
 extern int  
-Status_Transfer_Under_Condition(const Status *status, Condition cond);
+Status_Transfer_Under_Condition(const Status status, E cond);
 
-void 
-freeEdge( Edge *edge );
 
-void freeNFA( NFA *nfa );
+void freeNFA( NFA nfa );
 
+
+#undef E
 #endif
